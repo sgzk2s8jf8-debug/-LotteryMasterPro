77abeda3682a45451d6780d4b2aff8291e12a4a3
sgzk2s8jf8-debug <sgzk2s8jf8@privaterelay.appleid.com>
2026-07-30 11:32:29 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "LotteryMasterPro-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js"
+];
+
+
+self.addEventListener(
+"install",
+event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache => {
+
+return cache.addAll(FILES);
+
+})
+
+);
+
+});
+
+
+
+self.addEventListener(
+"fetch",
+event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response => {
+
+return response ||
+fetch(event.request);
+
+})
+
+);
+
+});
